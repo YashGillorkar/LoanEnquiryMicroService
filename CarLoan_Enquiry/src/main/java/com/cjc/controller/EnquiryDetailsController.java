98e0cec23234c6acd3daa471850aedf24e0818cf
Yash Gillorkar <144929163+YashGillorkar@users.noreply.github.com>
2024-07-27 13:20:54 +0530

    Configure to frontEnd Project

--- CarLoan_Enquiry/src/main/java/com/cjc/controller/EnquiryDetailsController.java
+++ CarLoan_Enquiry/src/main/java/com/cjc/controller/EnquiryDetailsController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import com.cjc.model.CibilDetails;
 import com.cjc.model.EnquiryDetails;
 import com.cjc.serviceI.EnquiryDetailServiceI;
 
+@CrossOrigin("*")
 @RestController
 public class EnquiryDetailsController {
 
